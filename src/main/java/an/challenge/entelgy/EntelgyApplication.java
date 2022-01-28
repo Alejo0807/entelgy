95e66d487df28526e3f62a669d4a78089e7a6301
Alejandro NM <ale_14_nb@hotmail.com>
2022-01-27 21:50:30 -0500

    first commit

---
+++ src/main/java/an/challenge/entelgy/EntelgyApplication.java
@@ -0,0 +1,13 @@
+package an.challenge.entelgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntelgyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntelgyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/an/challenge/entelgy/EntelgyApplicationTests.java
@@ -0,0 +1,13 @@
+package an.challenge.entelgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntelgyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
